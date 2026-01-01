687edf57cd25c11406a40435d00118aa8f073100
abdulrehmancsf25-lgtm <abdulrehmanarshad680@gmail.com>
2026-01-01 20:48:26 +0500

    my projects

---
+++ Web/index.js
@@ -0,0 +1,66 @@
+/*function solve() {
+  var age = 19;
+  console.log(age);
+}
+solve();
+console.log(age);
+//let is accessible only in  block if it is written inside
+console.log(a);
+{
+  let a = 19;
+  //let a=20; is not possible(redefine)
+  //below is posssible
+  a = 20;
+  a = "abdul";
+  a = true;
+  console.log(a);
+}
+console.log(a);
+const b = 32;
+// b = 32; not possible to change the value of const
+console.log(b);
+let marks = 33.55;
+console.log(marks);
+//undefined data type e.g below
+let number = true;
+console.log(number);
+console.log(typeof number);
+let num1 = 5;
+let num2 = 7;
+//console.log(num1 - num2);
+//console.log(num2 % num1);
+//power is represented by **
+console.log(num1 ** num2);
+console.log(++num1);
+console.log(num2++);
+console.log(num2);
+//assignment operator is =
+let star1 = 3;
+let star2 = 6;
+//star1 = star1 + 7;
+star1 += 7;
+console.log(star1);
+//strict equality(===) vs loose equality(==)
+console.log("5" == 5);
+console.log("5" === 5);
+//console.log(4 != "5");
+//console.log(5 !== "3");
+//console.log(5 !== 5);
+//ternary operator
+let alpha = 5 == 5 ? "correct answer" : "incorrect answer";
+console.log(alpha);
+let ap = 1;
+let op = 5;
+let equal = ap <= op && ap == 1 ? "logic accepted" : "logic declined";
+console.log(equal);
+console.log(false || "abdul");
+//imp concept
+console.log(~0);
+console.log(~2);*/
+for (let a = 1; a <= 6; a++) {
+  if (a == 5) {
+    break;
+  } else {
+    console.log("apple");
+  }
+}
